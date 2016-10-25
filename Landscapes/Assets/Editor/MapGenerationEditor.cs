41561ada58bb88091add934fbd891ea366834512
dacostaleo <dacostaleo@gmail.com>
2016-10-25 17:29:59 +0200

    Tutorial Part1

---
+++ Landscapes/Assets/Editor/MapGenerationEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGeneration))]
+public class MapGenerationEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        MapGeneration mapGen = target as MapGeneration;
+
+        if (DrawDefaultInspector() && mapGen.refresh)
+        {
+            mapGen.GenerateMap();
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+
+    }
+}
---
+++ Landscapes/Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGeneration : MonoBehaviour {
+
+    public int mapWidth;
+    public int mapHeight;
+
+    public float noiseScale;
+
+    public bool refresh;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        DisplayMap display = FindObjectOfType<DisplayMap>();
+        display.DrawNoiseMap(noiseMap);
+    }
+	
+}
